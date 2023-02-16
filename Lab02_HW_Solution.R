9a832bea85bdeed6348341bde5de0c604eef63fb
saizanaandezana <binyam@vt.edu>
2023-02-16 12:39:16 -0500

    Lab03

---
+++ Lab02_HW_Solution.R
@@ -0,0 +1,242 @@
+# 
+# Since everything depends on the libraries you install
+# it is worthwhile loading them at the beginning
+#
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(ggplot2,dplyr,patchwork,rnoaa)
+pacman::p_load(operators,topmodel,DEoptim,soilDB,sp,curl,httr,
+               rnoaa,raster,shapefiles,rgdal,elevatr,terra,progress,lubridate)
+LabNo="/Lab03"
+#
+# Getting our organization on for where we want to put
+# Data, external programs, and our project files.
+# Things are going to get messy if we don't start issolating
+# our data files by Lab
+#
+myhomedir=Sys.getenv("HOME")
+datadir=paste0(myhomedir,"/data",LabNo)
+dir.create(datadir,recursive = T)
+srcdir=paste0(myhomedir,"/src")
+dir.create(srcdir,recursive = T)
+
+# Setting the directory for where the GitHub project exists. 
+# This depends on where you set up your git, and what you called it locally, 
+# but when you start a new git project, it will be the first directory you 
+# are placed in... or if later in the project:
+# WOOO HOOO... took me a few hours to find this function!
+# 
+mygitdir=rstudioapi::getActiveProject()
+mypdfdir=paste0(mygitdir,"/pdfs",LabNo)
+dir.create(mypdfdir)
+# 
+setwd(mygitdir)
+system("git config --global user.email 'drfuka@vt.edu' ") 
+system("git config --global user.name 'Daniel Fuka' ")
+system("git config pull.rebase false")
+#
+# This was already done before, and doesn't need to be repeated unless there
+# is an update to R or the EcoHydRology Package... but 
+#
+setwd(srcdir)
+system("svn checkout svn://scm.r-forge.r-project.org/svnroot/ecohydrology/"); 
+install.packages(c("ecohydrology/pkg/EcoHydRology/"),repos = NULL)
+pacman::p_load(EcoHydRology)
+
+setwd(datadir)
+
+myflowgage_id="0205551460"
+myflowgage=get_usgs_gage(myflowgage_id,begin_date = "2015-01-01",
+                           end_date = "2019-01-01")
+#
+# This is where some folks had issues... they forgot to check their 
+# watershed areas per the homework... though there were ways to fix
+# it later with lower resolution DEM pull
+#
+print(myflowgage$area)
+# For most watershed modelling purposes we normalize Q in mm/day for basins
+myflowgage$flowdata$Qmm = myflowgage$flowdata$flow/myflowgage$area/10^3
+
+# In the Lab02, we introduced you to a way to quickly get your WX Data 
+# for any location in the world way easier than traditional download and
+# parsing methods most old people use.
+#
+WXData=FillMissWX(declat=myflowgage$declat, declon=myflowgage$declon,
+                    StnRadius=30,minstns=10,date_min="2010-01-01",
+                    date_max="2023-02-01",targElev=1,
+                    method = "IDEW",alfa=2)
+
+BasinData=merge(WXData,myflowgage$flowdata,by.x="date",by.y="mdate")
+#
+# Setting the projection information for the specific location
+#
+proj4_utm = paste0("+proj=utm +zone=", trunc((180+myflowgage$declon)/6+1), " +datum=WGS84 +units=m +no_defs")
+
+# Lat/Lon (_ll) is much easier!
+proj4_ll = "+proj=longlat"
+
+# Now we will build our proj4strings which define our “Coordinate 
+# Reference Systems” or CRS in future geographic manipulations. 
+crs_ll=CRS(proj4_ll)
+crs_utm=CRS(proj4_utm)
+#
+# Double chec
+
+latlon <- cbind(myflowgage$declon,myflowgage$declat)
+myflowgage$gagepoint_ll <- SpatialPoints(latlon)
+proj4string(myflowgage$gagepoint_ll)=proj4_ll
+myflowgage$gagepoint_utm=spTransform(myflowgage$gagepoint_ll,crs_utm)
+# Open up maps.google.com to guesstimate area/lengths
+url=paste0("https://www.google.com/maps/@",
+             myflowgage$declat,",",myflowgage$declon,",18z")
+browseURL(url)
+# We are going to over estimate our area
+# For our search we are going to multiply the area by 6 and
+# to get the distance
+searchlength=sqrt(myflowgage$area*8)*1000 
+pourpoint=SpatialPoints(myflowgage$gagepoint_utm@coords,proj4string = crs_utm)
+bboxpts=myflowgage$gagepoint_utm@coords
+bboxpts=rbind(bboxpts,bboxpts+searchlength)
+bboxpts=rbind(bboxpts,bboxpts-searchlength)
+bboxpts
+bboxpts=rbind(bboxpts,c(min(bboxpts[,1]),max(bboxpts[,2])))
+bboxpts=rbind(bboxpts,c(max(bboxpts[,1]),min(bboxpts[,2])))
+bboxpts
+bboxpts=SpatialPoints(bboxpts,proj4string = crs_utm)
+# From Lab04, get your DEM
+mydem=get_aws_terrain(locations=bboxpts@coords, 
+                        z = 12, prj = proj4_utm,src ="aws",expand=1)
+res(mydem)
+plot(mydem)
+plot(bboxpts,add=T)
+plot(pourpoint,add=T,col="red")
+
+# Write our raster to a geotiff file that can be used with
+# OS level hydrological models 
+writeRaster(mydem,filename = "mydem.tif",overwrite=T)
+# Our quick intro to terminal where the cloud offerings are usually Linux
+# ls; cd ~; pwd;  # Linux/Mac 
+# dir; cd ; # Windows
+
+#
+# I am going to set two different zoom levels so I can inspect 
+# the TauDEM Processing below.
+#
+
+zoomext=myflowgage$gagepoint_utm@coords
+zoomext=rbind(zoomext,zoomext+res(mydem)*100)
+zoomext=rbind(zoomext,zoomext-res(mydem)*100)
+zoomext=SpatialPoints(zoomext,proj4string = crs_utm)  
+zoomext2=myflowgage$gagepoint_utm@coords
+zoomext2=rbind(zoomext2,zoomext2+res(mydem)*10)
+zoomext2=rbind(zoomext2,zoomext2-res(mydem)*10)
+zoomext2=SpatialPoints(zoomext2,proj4string = crs_utm)  
+zoom(mydem,ext=zoomext2)
+plot(pourpoint,add=T,col="red")
+
+# If you already installed this in your ~/src directory and it 
+# worked... you really 
+# cd ~/src/      # Set your directory to your home directory
+# git clone https://github.com/dtarb/TauDEM.git
+# mkdir ~/src/TauDEM/bin
+# cd ~/src/TauDEM/src
+# sed -i -e 's/MPI_Type_struct/MPI_Type_create_struct/g' linklib.h
+## yes, this next line is very small font, but it is one line so...
+# sed -i -e 's/MPI_Type_extent(MPI_LONG, \&extent)/MPI_Aint lb\;MPI_Type_get_extent(MPI_LONG, \&lb, \&extent)/g' linklib.h
+## Now let's try make again!
+# make
+
+rm("old_path")
+old_path <- Sys.getenv("PATH")
+old_path
+
+if( ! grepl("~/src/TauDEM/bin",old_path)){
+  Sys.setenv(PATH = paste(old_path,
+                            paste0(Sys.getenv("HOME"),"/src/TauDEM/bin"), 
+                            sep = ":"))
+}
+
+system("mpirun aread8")
+
+setwd(datadir)
+z=raster("mydem.tif")
+plot(z)
+
+# Pitremove
+system("mpiexec -n 2 pitremove -z mydem.tif -fel mydemfel.tif")
+fel=raster("mydemfel.tif")
+plot(fel-z)
+
+
+# D8 flow directions
+system("mpiexec -n 2 d8flowdir -p mydemp.tif -sd8 mydemsd8.tif -fel mydemfel.tif",show.output.on.console=F,invisible=F)
+p=raster("mydemp.tif")
+plot(p)
+sd8=raster("mydemsd8.tif")
+plot(sd8)
+
+# Contributing area
+system("mpiexec -n 2 aread8 -p mydemp.tif -ad8 mydemad8.tif")
+ad8=raster("mydemad8.tif")
+plot(log(ad8))
+zoom(log(ad8),ext=zoomext2)
+plot(pourpoint,add=T)
+
+# Grid Network 
+system("mpiexec -n 2 gridnet -p mydemp.tif -gord mydemgord.tif -plen mydemplen.tif -tlen mydemtlen.tif")
+gord=raster("mydemgord.tif")
+plot(gord)
+zoom(gord,ext=zoomext2)
+
+# DInf flow directions
+system("mpiexec -n 2 dinfflowdir -ang mydemang.tif -slp mydemslp.tif -fel mydemfel.tif",show.output.on.console=F,invisible=F)
+ang=raster("mydemang.tif")
+plot(ang)
+slp=raster("mydemslp.tif")
+plot(slp)
+
+# Dinf contributing area
+system("mpiexec -n 2 areadinf -ang mydemang.tif -sca mydemsca.tif")
+sca=raster("mydemsca.tif")
+plot(log(sca))
+zoom(log(sca),ext=zoomext2)
+
+# Threshold
+system("mpiexec -n 2 threshold -ssa mydemad8.tif -src mydemsrc.tif -thresh 100")
+src=raster("mydemsrc.tif")
+plot(src)
+zoom(src,ext=zoomext2)
+
+outlet=SpatialPointsDataFrame(myflowgage$gagepoint_utm,
+                                data.frame(Id=c(1),outlet=paste("outlet",1,sep="")))
+writeOGR(outlet,dsn=".",layer="approxoutlets",
+           driver="ESRI Shapefile", overwrite_layer=TRUE)
+#
+
+# Move Outlets
+system("mpiexec -n 2 moveoutletstostrm -p mydemp.tif -src mydemsrc.tif -o approxoutlets.shp -om outlet.shp")
+
+approxpt=readOGR("approxoutlets.shp")
+plot(approxpt,add=T, col="blue")
+outpt=readOGR("outlet.shp")
+plot(outpt,add=T, col="red")
+
+# Contributing area upstream of outlet
+# Now that we know the location of an outlet, we can isolate our basin 
+#
+system("mpiexec -n 2 aread8 -p mydemp.tif -o outlet.shp -ad8 mydemssa.tif")
+ssa=raster("mydemssa.tif")
+plot(ssa) 
+
+# Threshold
+system("mpiexec -n 2 threshold -ssa mydemssa.tif -src mydemsrc1.tif -thresh 2000")
+src1=raster("mydemsrc1.tif")
+plot(src1)
+zoom(src1,ext=zoomext2)
+
+# Stream Reach and Watershed
+system("mpiexec -n 2 streamnet -fel mydemfel.tif -p mydemp.tif -ad8 mydemad8.tif -src mydemsrc1.tif -o outlet.shp -ord mydemord.tif -tree mydemtree.txt -coord mydemcoord.txt -net mydemnet.shp -w mydemw.tif")
+plot(raster("mydemord.tif"))
+zoom(raster("mydemord.tif"),ext=zoomext2)
+plot(raster("mydemw.tif"))
+
+
