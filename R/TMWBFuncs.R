20fb4664db13d680e77db75f647eb836fa9e04bd
saizanaandezana <binyam@vt.edu>
2023-02-16 13:56:59 -0500

    my TMWBFuncs

---
+++ R/TMWBFuncs.R
@@ -0,0 +1,18 @@
+# soil wetting function
+soilwetting<-function(AWprev,dP_func,AWC_func){
+  AW_func<-AWprev+dP_func
+  excess_func<-0.0
+  c(AW_func,excess_func)
+} 
+# soil drying function
+soildrying<-function(AWprev,dP_func,AWC_func){
+  AW_func=AWprev*exp(dP_func/AWC_func)
+  excess_func<-0.0
+  c(AW_func,excess_func)
+}
+# soil_wetting_above_capacity function
+soil_wetting_above_capacity<-function(AWprev,dP_func,AWC_func){
+  AW_func<-AWC_func
+  excess_func<-AWprev+dP_func-AWC_func
+  c(AW_func,excess_func)
+}
